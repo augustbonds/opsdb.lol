883461cb9abf95db47536063a658345636685db4
August Bonds <hello@augustbonds.com>
2024-02-24 22:03:23 +0100

    comment out pages backend endpoint

--- app/people/index.ts
+++ app/people/index.ts
@@ -1,32 +1,32 @@
-// pages/api/people/index.ts
-import type { NextApiRequest, NextApiResponse } from 'next'
-import { getSession } from 'next-auth/react';
-import { prisma } from '../api/db'; // Assume you've set up Prisma
+// // pages/api/people/index.ts
+// import type { NextApiRequest, NextApiResponse } from 'next'
+// import { getSession } from 'next-auth/react';
+// import { prisma } from '../api/db'; // Assume you've set up Prisma
 
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  const session = await getSession({ req });
+// export default async function handler(
+//   req: NextApiRequest,
+//   res: NextApiResponse
+// ) {
+//   const session = await getSession({ req });
 
-  if (!session) {
-    return res.status(401).json({ message: 'You must be logged in to do this.' });
-  }
+//   if (!session) {
+//     return res.status(401).json({ message: 'You must be logged in to do this.' });
+//   }
 
-  if (req.method === 'POST') {
-    // Add validation for req.body
-    const { name } = req.body;
-    const person = await prisma.Typee.create({
-      data: {
-        name,
-        createdByUserId: session.user.id, // Assuming you have user IDs linked in session
-      },
-    });
-    return res.status(200).json(person);
-  }
+//   if (req.method === 'POST') {
+//     // Add validation for req.body
+//     const { name } = req.body;
+//     const person = await prisma.Typee.create({
+//       data: {
+//         name,
+//         createdByUserId: session.user.id, // Assuming you have user IDs linked in session
+//       },
+//     });
+//     return res.status(200).json(person);
+//   }
 
-  // Handle other methods or return 405 for not allowed
-  res.setHeader('Allow', ['POST']);
-  res.status(405).end(`Method ${req.method} Not Allowed`);
-}
+//   // Handle other methods or return 405 for not allowed
+//   res.setHeader('Allow', ['POST']);
+//   res.status(405).end(`Method ${req.method} Not Allowed`);
+// }
